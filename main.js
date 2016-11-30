d8397dc5e679bc46df5d7e668aaf6963d8fc32cf
j-west <westja89@gmail.com>
2016-11-29 20:48:39 -0600

    Fix mistake where first fib number was not displaying

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ function fibonacci(num1, num2) {
   if (num3 > 499) {
     console.log(num2);
     return console.log("That's all folks");
-   }
+  }else if (num1 === 1 && num2 === 1 ) {
+    console.log(num1);
+  }
     console.log(num2);
     return fibonacci(num2, num3);
 
